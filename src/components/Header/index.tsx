42de54d420dda0dc1c0b14da832d10245cb2fc5f
Talita Azevedo <talita.azevedo360@gmail.com>
2021-08-09 02:44:24 -0300

    criando html

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import styles from './header.module.scss';
+import common from '../../styles/common.module.scss';
+
+export default function Header(): JSX.Element {
+  // TODO!
+  return (
+    <header className={`${styles.header} `}>
+      <div className={common.container}>
+        <Link href="/">
+          <img src="/logo.svg" alt="logo" />
+        </Link>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 
+import Head from 'next/head';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +25,67 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  // TODO
+  return (
+    <>
+      <Head>
+        <title> Home | space Traveling.</title>
+      </Head>
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          <a>
+            <strong> Como Utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.authorGroup}>
+              <div>
+                <img src="/images/calendar.svg" alt="" srcSet="" />
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <img src="/images/user.svg" alt="" srcSet="" />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong> Como Utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.authorGroup}>
+              <div>
+                <img src="/images/calendar.svg" alt="" srcSet="" />
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <img src="/images/user.svg" alt="" srcSet="" />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </a>
+          <a>
+            <strong> Como Utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div className={styles.authorGroup}>
+              <div>
+                <img src="/images/calendar.svg" alt="" srcSet="" />
+                <time>15 Mar 2021</time>
+              </div>
+              <div>
+                <img src="/images/user.svg" alt="" srcSet="" />
+                <span>Joseph Oliveira</span>
+              </div>
+            </div>
+          </a>
+        </div>
+      </main>
+      <div className={`${styles.buttonLoadMore}${commonStyles.container}`}>
+        {' '}
+        Carregar Mais Posts
+      </div>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
